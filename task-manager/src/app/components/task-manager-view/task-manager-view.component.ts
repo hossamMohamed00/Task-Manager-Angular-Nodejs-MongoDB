ce493b4066a7880e186540a433a6e3b69a3b4ec3
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2021-12-21 20:07:16 +0200

    Update loading animation period

--- task-manager/src/app/components/task-manager-view/task-manager-view.component.ts
+++ task-manager/src/app/components/task-manager-view/task-manager-view.component.ts
@@ -14,7 +14,7 @@ export class TaskManagerViewComponent implements OnInit {
     setTimeout(() => {
       this.hideLoader()
       this.showTaskManagerContainer()
-    }, 0)
+    }, 2000)
   }
 
   showTaskManagerContainer() {
